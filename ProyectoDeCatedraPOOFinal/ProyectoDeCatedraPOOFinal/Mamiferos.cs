f54261898b1014d55d2421fa44d1f6ccaeed7c93
luispablovelasco <pablo.velascoflores@gmail.com>
2021-11-01 21:08:59 -0600

    Creacion de clase abstracta y clases derivadas, falta atributos unicos y restricciones

---
+++ ProyectoDeCatedraPOOFinal/ProyectoDeCatedraPOOFinal/Mamiferos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDeCatedraPOOFinal
+{
+    /*
+        INTEGRANTES DEL EQUIPO
+
+            - Ayala Acevedo, Diego Javier -------- AA210072
+            - Alvarado, Anderson Meléndez -------- MA201409
+            - Elías Pérez, Karletty Carolina ----- EP210584
+            - Matínez Soto, Adriana Guadalupe ---- MS211714
+            - Velasco Flores, Luis Pablo --------- VF202313
+
+    */
+
+    class Mamiferos : Animales
+    {
+
+
+    }
+}
---
+++ ProyectoDeCatedraPOOFinal/ProyectoDeCatedraPOOFinal/Peces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDeCatedraPOOFinal
+{
+    /*
+        INTEGRANTES DEL EQUIPO
+
+            - Ayala Acevedo, Diego Javier -------- AA210072
+            - Alvarado, Anderson Meléndez -------- MA201409
+            - Elías Pérez, Karletty Carolina ----- EP210584
+            - Matínez Soto, Adriana Guadalupe ---- MS211714
+            - Velasco Flores, Luis Pablo --------- VF202313
+
+    */
+
+    class Peces : Animales
+    {
+    }
+}
